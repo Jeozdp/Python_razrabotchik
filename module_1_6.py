247851761cdc3290d3274940050001b5e021e902
Jeozdp <kalvinjulia6@gmail.com>
2024-10-11 22:20:58 +0800

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,12 +1,11 @@
 my_dict = {'Lera': 1998,'Max': 2001, 'Sasha': 2005}
 print(my_dict)
 print(my_dict['Sasha'])
-my_dict['Osmond'] = 1976
-print(my_dict['Osmond'])
+print(my_dict.get('Osmond', 1998))
 my_dict.update({'Blake': 2010,
                 'Sybil':1992})
 print(my_dict)
-del my_dict['Lera']
+my_dict.pop('Lera')
 print(my_dict)
 #print(my_dist['Lera'])
 my_set = {1, 2, 2, 3, 4, 4, 5, 'a', 'a', 'b', 'c', 'c', 1.5, 1.5, True, False, True}
